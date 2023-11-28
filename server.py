595d87fd6372db0b5bedd2cb58713845fb7279b7
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2023-11-28 14:42:49 +0200

    Refactor file processing functions and constant

--- server.py
+++ server.py
@@ -1,87 +1,66 @@
-from models import proccesVideoAudioFile, proccesYoutubeLinkVideo
-from flask import Flask, render_template, jsonify, request, url_for
+from flask import Flask, render_template, jsonify, request
+from models import processVideoAudioFile, processYoutubeLinkVideo
 import os
 
 app = Flask(__name__)
-# Define the folder where uploaded videos will be stored
-UPLOAD_Videos_FOLDER_PATH = "./uploaded_videos"
-# Define the folder where uploaded audios will be stored
-UPLOAD_Audios_FOLDER_PATH = "./uploaded_audios"
 
-# Create the upload videos folder if it doesn't exist
-if not os.path.exists(UPLOAD_Videos_FOLDER_PATH):
-    os.makedirs(UPLOAD_Videos_FOLDER_PATH)
+# Configuration
+UPLOAD_VIDEOS_FOLDER = "./uploaded_videos"
+UPLOAD_AUDIOS_FOLDER = "./uploaded_audios"
+PORT = 8000
 
-# Create the upload audios folder if it doesn't exist
-if not os.path.exists(UPLOAD_Audios_FOLDER_PATH):
-    os.makedirs(UPLOAD_Audios_FOLDER_PATH)
+# Create necessary directories
+def create_directory(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+create_directory(UPLOAD_VIDEOS_FOLDER)
+create_directory(UPLOAD_AUDIOS_FOLDER)
 
 @app.route("/", methods=["GET"])
 def index():
+    """Render the index page."""
     return render_template("index.html")
 
+def process_file(file, folder_path, process_function):
+    """Generic file processing function."""
+    try:
+        file_path = os.path.join(folder_path, file.filename)
+        file.save(file_path)
+        result = process_function(file_path)
+        return jsonify({"data": result})
+    except Exception as e:
+        print(f"Error {type(e)} occurred: {e}")
+        return jsonify({"data": "Error processing file. Please try again."})
 
 @app.route("/getAudioFile", methods=["POST"])
-def audioFileProcessing():
-    print(f"request.files : {request.files}")
-    if "audio" in request.files:
-        audio = request.files["audio"]
-        if audio.filename != "":
-            try:                
-                audio_path = os.path.join(UPLOAD_Audios_FOLDER_PATH, audio.filename)
-                audio.save(audio_path)
-
-                result = proccesVideoAudioFile(audio_path)            
-                return jsonify({"data":result})
-            except Exception as error:
-                print(f"Error {type(error)} occurred while processing the audio: {error}")
-                return {"data": "Please, Re-Upload the audio file !!!"}
-        else:
-            return {"data": "Please, Re-Upload the audio file !"}
-    else:
-        return {"data": "Only audios are allowed to be uploaded !"}
-
+def process_audio():
+    """Process uploaded audio files."""
+    audio = request.files.get("audio")
+    if audio and audio.filename:
+        return process_file(audio, UPLOAD_AUDIOS_FOLDER, processVideoAudioFile)
+    return jsonify({"data": "Invalid audio file. Please upload a valid file."})
 
 @app.route("/getVideoFile", methods=["POST"])
-def videoFileProcessing():
-    if "video" in request.files:
-        video = request.files["video"]
-        if video.filename != "":
-            try:                
-                video_path = os.path.join(UPLOAD_Videos_FOLDER_PATH, video.filename)
-                video.save(video_path)
-                print(f" 1video file path {video_path}")
-                result = proccesVideoAudioFile(video_path)            
-                return jsonify({"data":result})
-            except Exception as error:
-                print(f"Error {type(error)} occurred while processing the video: {error}")
-                return {"data": "Please, Re-Upload the video file !!!"}
-        else:
-            return {"data": "Please, Re-Upload the video file !"}
-    else:
-        return {"data": "Only videos are allowed to be uploaded !"}
-
+def process_video():
+    """Process uploaded video files."""
+    video = request.files.get("video")
+    if video and video.filename:
+        return process_file(video, UPLOAD_VIDEOS_FOLDER, processVideoAudioFile)
+    return jsonify({"data": "Invalid video file. Please upload a valid file."})
 
 @app.route("/getYoutubeVideoLink", methods=["POST"])
-def videoLinkProcessing():
-    try:
-        videoLink = request.form["youtubeVideoLink"]
-    except:
-        videoLink = request.json.get("link")
-    if videoLink:
-        try:                
-            result = proccesYoutubeLinkVideo(videoLink)            
-            return jsonify({"data":result})                            
-        except Exception as error:
-            print(f"Error {type(error)} occurred while processing the youtube video link: {error}")
-            return {"data": "Please, Re-Upload the youtube video link !!!"}
+def process_youtube_link():
+    """Process YouTube video links."""
+    video_link = request.form.get("youtubeVideoLink", request.json.get("link"))
+    if video_link:
+        try:
+            result = processYoutubeLinkVideo(video_link)
+            return jsonify({"data": result})
+        except Exception as e:
+            print(f"Error {type(e)} occurred: {e}")
+            return jsonify({"data": "Error processing YouTube link. Please try again."})
+    return jsonify({"data": "Please provide a YouTube video link."})
 
-    else:
-        return {"data": "Please enter the youtube video link to be processed !"}
-
-port = 8070      
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0',debug=True,port=port)
-    print(f"Server is running at port {port}")
-
-    
+    app.run(host='0.0.0.0', debug=True, port=PORT)
